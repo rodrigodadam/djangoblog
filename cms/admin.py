f3fab5f6ab773a605a82a6e7cb877e53db16084a
rodrigodadam <rodrigodadam@gmail.com>
2021-12-06 16:20:07 +0000

    Add postadmin fields

--- cms/admin.py
+++ cms/admin.py
@@ -6,4 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+    
