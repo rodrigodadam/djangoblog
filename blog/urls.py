89a222c6951ce46ce42aafe50df59a51c0d5806e
rodrigodadam <rodrigodadam@gmail.com>
2021-12-07 09:36:10 +0000

    loop correction

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,4 @@
-"""blog URL Configuration
-
+"""codestar URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -18,5 +17,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("cms.urls"), name="cms-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
